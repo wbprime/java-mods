9cacccb9f4a4af8e4d90d06d73a6ca78ca407c06
Elvis Wang <mail@wangbo.im>
2019-10-22 14:40:56 +0800

    Clean old generated files

--- jooq-usecases/src/main/java/im/wangbo/java/usecases/jooq/GenerateTestingData.java
+++ jooq-usecases/src/main/java/im/wangbo/java/usecases/jooq/GenerateTestingData.java
@@ -1,11 +1,6 @@
 package im.wangbo.java.usecases.jooq;
 
-import javax.sql.DataSource;
-import org.h2.jdbcx.JdbcConnectionPool;
-import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
-import org.jooq.impl.DSL;
-import org.jooq.impl.DefaultDataType;
 
 /**
  * TODO Detail goes here.
@@ -15,40 +10,6 @@ import org.jooq.impl.DefaultDataType;
 public class GenerateTestingData {
 
     public static void main(String[] args) {
-        final String url = "jdbc:h2:./prepared.db";
-        final SQLDialect dialect = SQLDialect.H2;
-
-        final DataSource dataSource = JdbcConnectionPool.create(url, "jooq", "jOOQ@h2");
-        try (final DSLContext dsl = DSL.using(dataSource, dialect)) {
-            dsl.createTableIfNotExists("user")
-                .column("id",
-                    DefaultDataType.getDataType(dialect, Long.class)
-                        .identity(true)
-                        .nullable(false))
-                .column("name",
-                    DefaultDataType.getDataType(dialect, String.class)
-                        .nullable(false)
-                        .length(100))
-                .column("created_millis",
-                    DefaultDataType.getDataType(dialect, Long.class)
-                        .nullable(false))
-                .constraint(DSL.primaryKey("id"));
-            dsl.createTableIfNotExists("book")
-                .column("id",
-                    DefaultDataType.getDataType(dialect, Long.class)
-                        .identity(true)
-                        .nullable(false))
-                .column("name",
-                    DefaultDataType.getDataType(dialect, String.class)
-                        .nullable(false)
-                        .length(100))
-                .column("author_id",
-                    DefaultDataType.getDataType(dialect, Long.class)
-                        .nullable(false))
-                .column("created_millis",
-                    DefaultDataType.getDataType(dialect, Long.class)
-                        .nullable(false))
-                .constraint(DSL.primaryKey("id"));
-        }
+        SQLDialect.POSTGRES
     }
 }
