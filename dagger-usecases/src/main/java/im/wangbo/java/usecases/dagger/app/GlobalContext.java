515d25cbdfb0eb24763ffe4d3f87a31491dde4a5
Elvis Wang <mail@wangbo.im>
2019-10-28 17:47:57 +0800

    Add GlobalContext support

---
+++ dagger-usecases/src/main/java/im/wangbo/java/usecases/dagger/app/GlobalContext.java
@@ -0,0 +1,38 @@
+package im.wangbo.java.usecases.dagger.app;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.inject.Inject;
+
+/**
+ * TODO Detail goes here.
+ *
+ * Created at 2019-10-28 by Elvis Wang
+ */
+public class GlobalContext {
+
+    private final Map<String, String> env;
+
+    @Inject
+    public GlobalContext() {
+        final Map<String, String> env = System.getenv();
+        this.env = new HashMap<>(env);
+    }
+
+    public final String resolve(final String k) {
+        if (k.startsWith("$") && k.length() > 1) {
+            return get(k.substring(1));
+        } else {
+            return k;
+        }
+    }
+
+    public final String get(final String k) {
+        return env.getOrDefault(k, "");
+    }
+
+    public final void set(final String k, final String v) {
+        env.put(k, v);
+    }
+
+}
--- dagger-usecases/src/main/java/im/wangbo/java/usecases/dagger/echo/EchoCommand.java
+++ dagger-usecases/src/main/java/im/wangbo/java/usecases/dagger/echo/EchoCommand.java
@@ -1,6 +1,7 @@
 package im.wangbo.java.usecases.dagger.echo;
 
 import im.wangbo.java.usecases.dagger.app.Command;
+import im.wangbo.java.usecases.dagger.app.GlobalContext;
 import im.wangbo.java.usecases.dagger.app.Stdout;
 import java.util.List;
 import javax.inject.Inject;
@@ -13,9 +14,11 @@ import javax.inject.Inject;
 final class EchoCommand implements Command {
 
     private final Stdout stdout;
+    private final GlobalContext ctx;
 
     @Inject
-    EchoCommand(final Stdout stdout) {
+    EchoCommand(final GlobalContext ctx, final Stdout stdout) {
+        this.ctx = ctx;
         this.stdout = stdout;
     }
 
@@ -23,11 +26,11 @@ final class EchoCommand implements Command {
     public Status handleInput(final List<String> args) {
         final int n = args.size();
         for (int i = 0; i < n - 1; i++) {
-            stdout.print(args.get(i)).print(" ");
+            stdout.print(ctx.resolve(args.get(i))).print(" ");
         }
 
         if (n > 0) {
-            stdout.println(args.get(n - 1));
+            stdout.println(ctx.resolve(args.get(n - 1)));
         }
         return Status.SUCCEED;
     }
