2f99730652d5aaa8eec02c8dac0c9b7dc3405f6d
Elvis Wang <mail@wangbo.im>
2019-10-30 11:29:16 +0800

    guice: move code position of Scanner construction in MainApp

--- guice-usecases/src/main/java/im/wangbo/java/usecases/guice/MainApp.java
+++ guice-usecases/src/main/java/im/wangbo/java/usecases/guice/MainApp.java
@@ -9,13 +9,12 @@ import java.util.Scanner;
 public class MainApp {
 
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-
         final Injector injector = Guice.createInjector(new MainModule(
             "TODO", 1993, "TODO", "TODO"
         ));
         final CommandProcessor processor = injector.getInstance(CommandProcessor.class);
 
+        final Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             final Status status = processor.process(scanner.nextLine());
             if (status.equals(Status.QUITING)) {
