3ad97fcaeb34cec11fa1e2d57d0c62d5b181f78e
Elvis Wang <mail@wangbo.im>
2021-05-10 09:46:59 +0800

    Add antlr case

---
+++ antlr-usecases/src/main/java/im/wangbo/java/usecases/antlr4/MainApp.java
@@ -0,0 +1,10 @@
+package im.wangbo.java.usecases.antlr4;
+
+import java.util.logging.Logger;
+
+public class MainApp {
+    private static final Logger L = Logger.getLogger(MainApp.class.getName());
+
+    public static void main(String[] args) throws Exception {
+    }
+}
